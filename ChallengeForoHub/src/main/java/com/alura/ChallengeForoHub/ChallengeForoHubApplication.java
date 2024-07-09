eaefd2416df577d7ab49aaae2e643bf62b5a7774
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2024-07-08 19:38:38 -0500

    Iniciando proyecto

---
+++ ChallengeForoHub/src/main/java/com/alura/ChallengeForoHub/ChallengeForoHubApplication.java
@@ -0,0 +1,13 @@
+package com.alura.ChallengeForoHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeForoHubApplication.class, args);
+	}
+
+}
---
+++ ChallengeForoHub/src/test/java/com/alura/ChallengeForoHub/ChallengeForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.ChallengeForoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
